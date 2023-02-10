565826d883c8d75dd4f9f131f6ab69fdceb8cee4
Raimon Esteve <raimonesteve@gmail.com>
2023-02-10 12:07:35 +0100

    Nacex return and reference client #073059

--- shipment.py
+++ shipment.py
@@ -36,6 +36,11 @@ class NacexMixin(ModelSQL, ModelView):
         ('8', 'Puente urbano night'),
         ('9', 'Interdia aerial'),
         ], 'Nacex Freq Codigo', sort=False)
+    nacex_ret = fields.Selection([
+        ('N', 'N'),
+        ('S', 'S'),
+        ], 'Nacex Ret', sort=False)
+    nacex_ref_cli = fields.Char('Nacex Ref Cli')
 
     @staticmethod
     def get_nacex_envase():
@@ -59,6 +64,10 @@ class NacexMixin(ModelSQL, ModelView):
     @staticmethod
     def default_nacex_frec_codigo():
         return None
+
+    @staticmethod
+    def default_nacex_ret():
+        return 'N'
 
     @fields.depends('nacex_tip_ea', 'customer')
     def on_change_nacex_tip_ea(self):
@@ -169,10 +178,11 @@ class ShipmentOut(NacexMixin, metaclass=PoolMeta):
             #   'D', Destino: Factura la agencia de entrega del envío
             #   'T': Tercera: Factura una tercera agencia
             data['tip_cob'] = 'O'
-            data['ref_cli'] = code[:20]
+            data['ref_cli'] = shipment.nacex_ref_cli and shipment.nacex_ref_cli[:20] or code[:20]
             data['tip_env'] = shipment.nacex_envase or api.nacex_envase or '2'
             data['bul'] = str(packages)[:3].zfill(3)
             data['kil'] = str(weight)
+            data['nacex_ret'] = shipment.nacex_ret or 'N'
 
             # 1	Interdia or Puente Urbano: frequency 1 (morning)
             # 2	Interdia or Puente Urbano: frequency 2 (late)
